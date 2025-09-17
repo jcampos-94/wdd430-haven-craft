ceb639e73ed6490a6a2e036afde923315cdc725e
jcampos-94 <jcampos.94@hotmail.com>
2025-09-17 10:02:24 -0500

    Next.js app created

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <nav>
+        <Image alt="logo" src={''} width={1} height={2} />
+        <h1>Handcrafted Haven</h1>
+        <Link href={''}>Home</Link>
+        <Link href={''}>Profiles</Link>
+        <Link href={''}>Log In</Link>
+        <Link href={''}>Cart</Link>
+      </nav>
+      <main>
+        <Image alt="" src={''} width={1} height={2} />
+        <div>
+          <div id="product-card"></div>
+          <div id="product-card"></div>
+          <div id="product-card"></div>
+        </div>
+      </main>
+      <footer className={styles.footer}>WDD430 - Handcrafted Haven</footer>
+    </div>
+  );
+}
