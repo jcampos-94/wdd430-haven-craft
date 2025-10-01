d1c63f516a4c4d5f4342472004f2d108637877f9
gabrielariver <riv23011@byui.edu>
2025-09-30 19:59:29 -0500

    Updated CartPage with localStorage support and ProductPage add to cart in localStorage

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -2,8 +2,24 @@
 
 import Image from 'next/image';
 import styles from './ProductPage.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function ProductPage() {
+  const router = useRouter();
+
+  const handleAddToCart = () => {
+    const newProduct = {
+      name: "Wooden Owl",
+      price: 49.99,
+      image: "/images/filler_img_small.jpg",
+    };
+    // Add to cart in localStorage
+    const currentCart = JSON.parse(localStorage.getItem("cart") || "[]");
+    currentCart.push(newProduct);
+    localStorage.setItem("cart", JSON.stringify(currentCart));
+    router.push("/cart");
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Wooden Owl</h1>
@@ -34,7 +50,9 @@ export default function ProductPage() {
             Perfect for as a decorative piece on bedrooms and studies.
           </p>
           <p className={styles.price}>$49.99</p>
-          <button className={styles.button}>Add to Cart</button>
+          <button className={styles.button} onClick={handleAddToCart}>
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
