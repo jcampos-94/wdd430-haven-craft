5d51e06eaa0b5095f060ea93d4afc49e7cfdf3b2
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2025-10-07 06:00:30 -0400

    added seller dashboard

--- src/app/checkout/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Checkout() {
-    return (
-        <div>
-            <h1>Checkout and Shipping</h1>
-            <p>Provide your shipping details and choose a payment method to complete your order</p>
-        </div>
-    )
-}
