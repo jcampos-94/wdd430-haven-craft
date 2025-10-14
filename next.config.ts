af2076a6f38953386709c4c8bf0e24fee12ec57d
jcampos-94 <jcampos.94@hotmail.com>
2025-10-14 10:26:42 -0500

    added a new hostname

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
       },
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.visualmindcourses.com',
       },
     ],
   },
 };
 
 export default nextConfig;
-
