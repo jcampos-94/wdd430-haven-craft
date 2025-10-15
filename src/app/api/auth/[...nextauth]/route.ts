7c1df2eff5f5589d88bf17d0a7a032672081d10d
jcampos-94 <jcampos.94@hotmail.com>
2025-10-15 09:56:15 -0500

    attempt 3

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,20 @@
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
-export const authOptions = {
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async redirect(params: { url: string; baseUrl: string }) {
-      // Always redirect to your production domain
-      return params.url.startsWith(params.baseUrl)
-        ? params.url
-        : params.baseUrl;
+    async redirect({ url, baseUrl }) {
+      // Forces using the production domain in Vercel
+      return url.startsWith(baseUrl) ? url : baseUrl;
     },
   },
-};
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+});
