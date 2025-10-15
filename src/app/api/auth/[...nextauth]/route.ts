f92aef3c880c6871508999799757599e1a667ff2
jcampos-94 <jcampos.94@hotmail.com>
2025-10-15 09:48:43 -0500

    route edit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import NextAuth from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -9,6 +9,16 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-});
+  callbacks: {
+    async redirect(params: { url: string; baseUrl: string }) {
+      // Always redirect to your production domain
+      return params.url.startsWith(params.baseUrl)
+        ? params.url
+        : params.baseUrl;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
