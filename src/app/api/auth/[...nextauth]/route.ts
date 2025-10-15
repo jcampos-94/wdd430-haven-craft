32700158002a40ee338a059c600681460ce6aa7f
jcampos-94 <jcampos.94@hotmail.com>
2025-10-15 10:52:44 -0500

    final fix, I hope

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,24 @@
-import NextAuth from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
+// import NextAuth from 'next-auth';
+// import GitHubProvider from 'next-auth/providers/github';
 
-export const {
-  handlers: { GET, POST },
-  auth,
-} = NextAuth({
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      // Forces using the production domain in Vercel
-      return url.startsWith(baseUrl) ? url : baseUrl;
-    },
-  },
-});
+// export const {
+//   handlers: { GET, POST },
+//   auth,
+// } = NextAuth({
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_ID!,
+//       clientSecret: process.env.GITHUB_SECRET!,
+//     }),
+//   ],
+//   callbacks: {
+//     async redirect({ url, baseUrl }) {
+//       // Forces using the production domain in Vercel
+//       return url.startsWith(baseUrl) ? url : baseUrl;
+//     },
+//   },
+// });
+
+import { handlers } from '@/auth';
+
+export const { GET, POST } = handlers;
